e3ca0d47bdaf4ed416bb1c95522e8b1b1f17ec3e
dantheman <danogbo0@gmail.com>
2023-09-12 20:03:07 +0100

    previous commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,15 +1,21 @@
 const Task = require('../models/Task');
 
 
-const get_all_tasks = (req,res) => {
-    res.send('all items from the mvc ');
+const get_all_tasks = async (req,res) => {
+    try {
+        const get_task = await Task.findOne('64f9fcc7247f16b6e5f5e90a');
+        res.status(201).json({ get_task })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+    // res.send('all items from the mvc ');
 }
 
 const create_task = async (req,res) => {
 
     try {
         const task = await Task.create(req.body)
-        res.status('201').json({ task })
+        res.status(201).json({ task })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
